7e88a7cc4457b4d99526286b5dcf38fa566e79b8
Vladimir Danilov <voffka.danilov@gmail.com>
2022-10-06 18:55:24 +0300

    Binary search algorithm added

--- BInarySearch/Program.cs
+++ BInarySearch/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int BinarySearch(int[] array, int item)
+{
+    int low = 0;
+    int high = array.Length - 1;
+
+    while (low <= high)
+    {
+        int guess = (low + high) / 2;
+
+        if (array[guess] == item)
+            return guess;
+
+        if (array[guess] < item)
+            low = guess + 1;
+
+        if (array[guess] > item)
+            high = guess - 1;
+    }
+    return -1;
+}
+
+/*********************************
+            Test zone
+*********************************/
+int[] arr = new int[101];
+int i = 0;
+foreach (int j in arr)
+{
+    arr[i] = i;
+    i++;
+}
+Console.Write("[" + string.Join(" ", arr) + "]");
+Console.WriteLine();
+
+Console.WriteLine(BinarySearch(arr, 5));

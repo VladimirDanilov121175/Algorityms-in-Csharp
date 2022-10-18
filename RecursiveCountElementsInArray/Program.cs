f3b9f00ba0a49339f516e84a55fb56f53d7ef5c3
Vladimir Danilov <voffka.danilov@gmail.com>
2022-10-18 16:48:39 +0300

    Added recursive method for counting elements in arrays

---
+++ RecursiveCountElementsInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// Grokking algorithms, p. 85, exercise 4.2
+// Рекурсивная функция подсчета элементов в списке
+
+int CountElements(int[] array, int low, int high)
+{
+    if (array[low] == array[high])
+        return 1;
+
+    int mid = (high + low) / 2;
+    return CountElements(array, low, mid) + CountElements(array, mid + 1, high);
+}
+
+// Test zone
+int[] array = new int[] { 1, 2, 11, 4, 6, 10 };
+Console.WriteLine(CountElements(array, 0, array.Length - 1));
---
+++ SelectSort/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Сортировка выбором / Selection sort
+from the book "Grokking algorithms"
+ */
+
+int[] SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < array.Length; j++)
+            if (array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
+        int temp = array[minIndex];
+        array[minIndex] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+
+// Test zone
+int[] arr = { 12, 1, 9, 34, 25, 7, 138, 11, 46, 99 };
+Console.Write(string.Join(" ", SelectionSort(arr)));

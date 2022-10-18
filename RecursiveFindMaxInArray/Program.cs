0790dee5171705566a612f080204a453bbe59aad
Vladimir Danilov <voffka.danilov@gmail.com>
2022-10-18 17:57:40 +0300

    Recursive method for finding max number in array

---
+++ RecursiveFindMaxInArray/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Grokking algorithms, p. 85, exercise 4.2
+Рекурсивная функция нахождения максимального числа в списке */
+
+int Max(int[] array, int low, int high)
+{
+    if (high == low + 1)
+    {
+        if (array[high] > array[low])
+            return array[high];
+        else return array[low];
+    }
+
+    int mid = (high + low) / 2;
+
+    if (Max(array, low, mid) > Max(array, mid + 1, high))
+        return Max(array, low, mid);
+    else return Max(array, mid + 1, high);
+}
+
+// Test zone
+int[] array = {10, 7, 34, 5, -5, 13, 98, 11};
+Console.WriteLine(Max(array, 0, array.Length - 1));   // -> 98
